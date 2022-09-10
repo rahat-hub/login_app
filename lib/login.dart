83593860153d6b2155662ceb95e9341ca23f7818
Rahat <rahat.munta@gmail.com>
2022-09-10 13:13:28 +0600

    Add all

---
+++ lib/login.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios,
+          size: 20,
+          color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+                child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                )
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
