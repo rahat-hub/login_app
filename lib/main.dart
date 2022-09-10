b92729c890c50a1c71d5084af2a8ef5cdffb8a4a
Rahat <rahat.munta@gmail.com>
2022-09-10 13:09:28 +0600

    change add colors.

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/login.dart';
+import 'package:login_app/signup.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    )
+  );
+}
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: const [
+                  Text("F10 Solutions",style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                    fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('we develop Ideas Into Software',textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/f10.png")
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage())
+                      );
+                      },
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color:  Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupPage())
+                      );
+                    },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color:  Colors.red,
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Text(
+                      'Signup',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
